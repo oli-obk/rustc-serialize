6b5677063bc0a2f9216d4ffa06a98fe2d85d2d14
Oliver Schneider <git1984941651981@oli-obk.de>
2015-02-24 16:18:34 +0100

    encoder builds

--- src/xml.rs
+++ src/xml.rs
@@ -1,6 +1,387 @@
 #![allow(dead_code)]
 
 use std::iter::IntoIterator;
+use std::num::Int;
+use std::{fmt, error};
+
+#[derive(Copy, Debug)]
+pub enum EncoderError {
+    FmtError(fmt::Error),
+    BadHashmapKey,
+    BadStructName,
+    BadStructFieldName,
+    BadEnumName,
+}
+
+impl error::FromError<fmt::Error> for EncoderError {
+    fn from_error(err: fmt::Error) -> EncoderError { EncoderError::FmtError(err) }
+}
+
+pub type EncodeResult<T> = Result<T, EncoderError>;
+
+enum EncodingFormat {
+    Compact,
+    Pretty {
+        curr_indent: u32,
+        indent: u32
+    }
+}
+
+/// A structure for implementing serialization to JSON.
+pub struct Encoder<'a> {
+    writer: &'a mut (fmt::Write+'a),
+    format : EncodingFormat,
+    is_emitting_map_key: bool,
+}
+
+macro_rules! emit {
+    ($enc:ident,$e:expr) => {{
+        try!(write!($enc.writer, "{}", $e));
+        Ok(())
+    }}
+}
+
+macro_rules! try_emit {
+    ($enc:ident,$e:expr) => {
+        try!(write!($enc.writer, "{}", $e))
+    }
+}
+
+impl<'a> Encoder<'a> {
+    /// Creates a new encoder whose output will be written in human-readable
+    /// Xml to the specified writer
+    pub fn new_pretty(writer: &'a mut fmt::Write) -> Encoder<'a> {
+        Encoder {
+            writer: writer,
+            format: EncodingFormat::Pretty {
+                curr_indent: 0,
+                indent: 2,
+            },
+            is_emitting_map_key: false,
+        }
+    }
+
+    /// Creates a new encoder whose output will be written in compact
+    /// Xml to the specified writer
+    pub fn new(writer: &'a mut fmt::Write) -> Encoder<'a> {
+        Encoder {
+            writer: writer,
+            format: EncodingFormat::Compact,
+            is_emitting_map_key: false,
+        }
+    }
+
+    /// Set the number of spaces to indent for each level.
+    /// This is safe to set during encoding.
+    pub fn set_indent(&mut self, new_indent: u32) -> Result<(), ()> {
+        if let EncodingFormat::Pretty{ref mut curr_indent, ref mut indent} = self.format {
+            // self.indent very well could be 0 so we need to use checked division.
+            let level = curr_indent.checked_div(*indent).unwrap_or(0);
+            *indent = new_indent;
+            *curr_indent = level * *indent;
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+}
+
+fn escape_char(writer: &mut fmt::Write, ch: char) -> EncodeResult<()> {
+    let enc = match ch {
+        '>' => "&gt;",
+        '<' => "&lt;",
+        '&' => "&amp;",
+        '\'' => "&apos;",
+        '"' => "&quot;",
+        _ => unreachable!(),
+    };
+    try!(write!(writer, "{}", enc));
+    Ok(())
+}
+
+fn escape_str(writer: &mut fmt::Write, s: &str) -> EncodeResult<()> {
+    for c in s.chars() {
+        try!(escape_char(writer, c));
+    }
+    Ok(())
+}
+
+fn spaces(wr: &mut fmt::Write, n: u32) -> EncodeResult<()> {
+    let mut n = n as usize;
+    const BUF: &'static str = "                ";
+
+    while n >= BUF.len() {
+        try!(wr.write_str(BUF));
+        n -= BUF.len();
+    }
+
+    if n > 0 {
+        try!(wr.write_str(&BUF[..n]));
+    }
+    Ok(())
+}
+
+impl<'a> ::Encoder for Encoder<'a> {
+    type Error = EncoderError;
+
+    fn emit_nil(&mut self) -> EncodeResult<()> {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        try!(write!(self.writer, "null"));
+        Ok(())
+    }
+
+    fn emit_usize(&mut self, v: usize) -> EncodeResult<()> { emit!(self, v) }
+    fn emit_u64(&mut self, v: u64) -> EncodeResult<()> { emit!(self, v) }
+    fn emit_u32(&mut self, v: u32) -> EncodeResult<()> { emit!(self, v) }
+    fn emit_u16(&mut self, v: u16) -> EncodeResult<()> { emit!(self, v) }
+    fn emit_u8(&mut self, v: u8) -> EncodeResult<()> { emit!(self, v) }
+
+    fn emit_isize(&mut self, v: isize) -> EncodeResult<()> { emit!(self, v) }
+    fn emit_i64(&mut self, v: i64) -> EncodeResult<()> { emit!(self, v) }
+    fn emit_i32(&mut self, v: i32) -> EncodeResult<()> { emit!(self, v) }
+    fn emit_i16(&mut self, v: i16) -> EncodeResult<()> { emit!(self, v) }
+    fn emit_i8(&mut self, v: i8) -> EncodeResult<()> { emit!(self, v) }
+
+    fn emit_bool(&mut self, v: bool) -> EncodeResult<()> {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        if v {
+            emit!(self, "true")
+        } else {
+            emit!(self, "false")
+        }
+    }
+
+    fn emit_f64(&mut self, v: f64) -> EncodeResult<()> {
+        emit!(self, v)
+    }
+    fn emit_f32(&mut self, v: f32) -> EncodeResult<()> {
+        self.emit_f64(v as f64)
+    }
+
+    fn emit_char(&mut self, v: char) -> EncodeResult<()> {
+        escape_char(self.writer, v)
+    }
+    fn emit_str(&mut self, v: &str) -> EncodeResult<()> {
+        escape_str(self.writer, v)
+    }
+
+    fn emit_enum<F>(&mut self, _name: &str, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        f(self)
+    }
+
+    fn emit_enum_variant<F>(&mut self,
+                            name: &str,
+                            _id: usize,
+                            cnt: usize,
+                            f: F)
+                            -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        // Bunny => <Bunny/>
+        // Kangaroo(34,"William") => <Kangaroo><field>34</field><field>William</field></Kangaroo>}
+        try_emit!(self, "<");
+        if name.chars().any(|ch| is_bad_name_char(ch)) {
+            return Err(EncoderError::BadEnumName);
+        }
+        try!(escape_str(self.writer, name));
+        if cnt == 0 {
+            emit!(self, "/>")
+        } else {
+            if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+            if let EncodingFormat::Pretty{ref mut curr_indent, indent} = self.format {
+                try_emit!(self, ">\n");
+                *curr_indent += indent;
+                try!(spaces(self.writer, *curr_indent));
+            } else {
+                try_emit!(self, ">");
+            }
+            try!(f(self));
+            if let EncodingFormat::Pretty{ref mut curr_indent, indent} = self.format {
+                *curr_indent -= indent;
+                try!(write!(self.writer, "\n"));
+                try!(spaces(self.writer, *curr_indent));
+            }
+            try_emit!(self, "</");
+            try!(escape_str(self.writer, name));
+            emit!(self, ">")
+        }
+    }
+
+    fn emit_enum_variant_arg<F>(&mut self, idx: usize, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        self.emit_struct_field("field", idx, f)
+    }
+
+    fn emit_enum_struct_variant<F>(&mut self,
+                                   name: &str,
+                                   id: usize,
+                                   cnt: usize,
+                                   f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        self.emit_enum_variant(name, id, cnt, f)
+    }
+
+    fn emit_enum_struct_variant_field<F>(&mut self,
+                                         _: &str,
+                                         idx: usize,
+                                         f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        self.emit_enum_variant_arg(idx, f)
+    }
+
+
+    fn emit_struct<F>(&mut self, name: &str, len: usize, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        try_emit!(self, "<");
+        if name.chars().any(|ch| is_bad_name_char(ch)) {
+            return Err(EncoderError::BadStructName);
+        }
+        try_emit!(self, name);
+        if len == 0 {
+            emit!(self, "/>")
+        } else {
+            try_emit!(self, ">");
+            if let EncodingFormat::Pretty{ref mut curr_indent, indent} = self.format {
+                *curr_indent += indent;
+            }
+            try!(f(self));
+            if let EncodingFormat::Pretty{ref mut curr_indent, indent} = self.format {
+                *curr_indent -= indent;
+                try_emit!(self, "\n");
+                try!(spaces(self.writer, *curr_indent));
+            }
+            try_emit!(self, "</");
+            try_emit!(self, name);
+            emit!(self, ">")
+        }
+    }
+
+    fn emit_struct_field<F>(&mut self, name: &str, _: usize, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        if name.chars().any(|ch| is_bad_name_char(ch)) {
+            return Err(EncoderError::BadStructFieldName);
+        }
+        try_emit!(self, "<");
+        try_emit!(self, name);
+        try_emit!(self, ">");
+        try!(f(self));
+        try_emit!(self, "</");
+        try_emit!(self, name);
+        try_emit!(self, ">");
+        if let EncodingFormat::Pretty{ref curr_indent, ..} = self.format {
+            try_emit!(self, "\n");
+            try!(spaces(self.writer, *curr_indent));
+        }
+        Ok(())
+    }
+
+    fn emit_tuple<F>(&mut self, len: usize, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        self.emit_seq(len, f)
+    }
+    fn emit_tuple_arg<F>(&mut self, idx: usize, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        self.emit_seq_elt(idx, f)
+    }
+
+    fn emit_tuple_struct<F>(&mut self, _: &str, len: usize, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        self.emit_seq(len, f)
+    }
+    fn emit_tuple_struct_arg<F>(&mut self, idx: usize, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        self.emit_seq_elt(idx, f)
+    }
+
+    fn emit_option<F>(&mut self, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        f(self)
+    }
+    fn emit_option_none(&mut self) -> EncodeResult<()> {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        self.emit_nil()
+    }
+    fn emit_option_some<F>(&mut self, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        f(self)
+    }
+
+    fn emit_seq<F>(&mut self, len: usize, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        if len > 0 {
+            try!(f(self));
+        }
+        Ok(())
+    }
+
+    fn emit_seq_elt<F>(&mut self, _: usize, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        if let EncodingFormat::Pretty{ref mut curr_indent, ..} = self.format {
+            try!(write!(self.writer, "\n"));
+            try!(spaces(self.writer, *curr_indent));
+        }
+        f(self)
+    }
+
+    fn emit_map<F>(&mut self, len: usize, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        if len > 0 {
+            try!(f(self));
+        }
+        Ok(())
+    }
+
+    fn emit_map_elt_key<F>(&mut self, _: usize, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        if let EncodingFormat::Pretty{curr_indent, ..} = self.format {
+            try!(write!(self.writer, "\n"));
+            try!(spaces(self.writer, curr_indent));
+        }
+        self.is_emitting_map_key = true;
+        try!(f(self));
+        self.is_emitting_map_key = false;
+        Ok(())
+    }
+
+    fn emit_map_elt_val<F>(&mut self, _idx: usize, f: F) -> EncodeResult<()> where
+        F: FnOnce(&mut Encoder<'a>) -> EncodeResult<()>,
+    {
+        if self.is_emitting_map_key { return Err(EncoderError::BadHashmapKey); }
+        f(self)
+    }
+}
+
 
 /*
 
